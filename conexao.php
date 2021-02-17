4487cd7558de00514f2423f2fe22f284dafa229e
Jeferson Ferreira <69856856+jefender@users.noreply.github.com>
2021-02-17 13:31:50 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,9 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:8100");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+$pdo = new PDO("mysql:host=localhost:3307;dbname=listacompra", "root", "usbw");
+
+?>
